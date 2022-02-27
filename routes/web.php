21400c1f99b84583e6d72dece991238b33985998
Chikavi1 <chikavi@hotmail.com>
2022-02-27 10:52:04 -0600

    project init

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SupplierController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/suppliers',[SupplierController::class, 'index']);
+Route::get('/suppliers/create',[SupplierController::class, 'create']);
+Route::post('/suppliers/store',[SupplierController::class, 'store']);
+Route::patch('/suppliers/update',[SupplierController::class, 'update'])->name('suppliers.update');
+Route::get('/suppliers/edit/{id}',[SupplierController::class, 'edit'])->name('suppliers.edit');
+Route::get('/suppliers/show/{id}',[SupplierController::class, 'show'])->name('suppliers.show');
+Route::delete('/suppliers/destroy',[SupplierController::class, 'destroy'])->name('suppliers.destroy');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
