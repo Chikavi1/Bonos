a9d346c30c65133179f6501536bc6e619ed6963f
Chikavi1 <chikavi@hotmail.com>
2022-05-04 20:14:06 -0500

    add landing page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,24 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
+    <div class="row ">
+        <div class="col-md-4">
+            <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src="../public/pattern.png" alt="Card image cap">
+                <div class="card-body">
+                    <p class="card-text">Hola, <a href="/profile">{{ $user->name }}</a>.</p>
+                </div>
+            </div>
+        </div>
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Movimientos</div>
-                <p>
-                    Bienvenid@ {{ $user->name }} {{ $user->type }}
-
-                </p>
-
+                <div class="card-header">
+                    Premios
+                </div>
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
+                    <h5 class="card-title">Descubre los premios que tenemos para ti.</h5>
+                    <p class="card-text">Tus compras generan puntos, averigua cuales son los premios que tenemos disponibles para ti.</p>
+                    <a href="/rewards"class="btn btn-primary">Ver Premios</a>
                 </div>
             </div>
         </div>
