4a98daee9059fbdbdf6ef4b3935b51f4ee62dfe4
Chikavi1 <chikavi@hotmail.com>
2022-05-04 20:15:00 -0500

    orden en layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,20 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" />
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet"
+/>
+<!-- Google Fonts -->
+<link
+  href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+  rel="stylesheet"
+/>
+<!-- MDB -->
+<link
+  href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/4.0.0/mdb.min.css"
+  rel="stylesheet"
+/>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </head>
 <body>
@@ -22,10 +36,15 @@
         <li class="nav-item">
           <p class="nav-link active" style="color:#17202F;font-weight:bold;">{{Auth::user()->points}} Puntos</p>
         </li>
+        @endif
+
+        @if(Auth::user()->type != 2)
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="/rewards">Premios</a>
         </li>
-        @elseif(Auth::user()->type == 3)
+        @endif
+
+        @if(Auth::user()->type == 3)
         <li class="nav-item">
           <a class="nav-link" href="/movements">Generar Movimiento</a>
         </li>
@@ -46,8 +65,11 @@
     </div>
   </div>
 </nav>
-<div class="container">
+<div class="container mt-4">
     @yield('content')
 </div>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<script src="{{ asset('/vendors/ckeditor/ckeditor.js') }}"></script>
 </body>
 </html>
